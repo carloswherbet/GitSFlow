f29ccbc7ca59f7c4e0ed3533a2b1231170726087
Carlos <carloswherbet@gmail.com>
2021-01-04 08:25:01 -0300

    Add news features and created 0.6.0 version

--- lib/GitLab/user.rb
+++ lib/GitLab/user.rb
@@ -6,7 +6,9 @@ class GitLab::User
   end
 
   def self.me 
-    GitLab.request_get("projects/#{$GITLAB_PROJECT_ID}/users?search=#{$GITLAB_EMAIL}")[0]
+    user = GitLab.request_get("projects/#{$GITLAB_PROJECT_ID}/users?search=#{$GITLAB_EMAIL}")[0] 
+    return user if user 
+    raise "Who are you?! \nVerify your data em .env"
   end
 
   def self.all
